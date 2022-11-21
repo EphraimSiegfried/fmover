48c72932e4f2b027e59f3b30e8d5bee461bf8e81
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2022-11-21 08:31:50 +0100

    Add proper destination folder for configurations

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 from setuptools import setup, find_packages
-import pathlib
-
-here = pathlib.Path(__file__).parent.resolve()
-long_description = (here, "README.md").read_text(encoding="utf-8")
 
 setup(
     name='file-mover',
     version='0.0.1',
     description='Move files based on file properties and given criteria',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     author='Ephraim Siegfried',
     author_email='ephraim.siegfried@hotmail.com',
-    package_dir={'': 'src'},
+    package_dir={'src': 'src'},
     python_requires=">=3.6.0",
     packages=find_packages(exclude=["tests"]),
     classifiers=[
@@ -20,6 +14,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Topic :: Desktop Environment :: File Managers"
     ],
-    entrypoints={"console_scripts":["file-mover = src.__main__:main"]},
-    install_requires=["notifypy>=1.0.3.0", "osxmetadata>=1.2.2"]
+    entry_points={"console_scripts":["file-mover = src.__main__:main"]},
+    install_requires=["notifypy>=1.0.3.0", "osxmetadata>=1.2.2, appdirs~=1.0.3.0"]
 )
--- src/configs.py
+++ src/configs.py
@@ -1,7 +1,46 @@
 import json
 import os
 
-CONFIGS_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "configurations")
+import appdirs
+
+path_to_data_dir = appdirs.user_data_dir("file-mover", "Ephraim Siegfried")
+CONFIGS_DIR = os.path.join(path_to_data_dir, "configurations")
+default_config = {
+    "COMMAND": [
+        {"FILE_EXTENSION(*)": "FILE_EXTENSION(*)"}],
+    "WHERE_FROM": {},
+    "NAME": {},
+    "FILE_EXTENSION": {
+        ".pdf": "./PDF",
+        ".ps": "./PDF",
+        ".jpg": "./Photos",
+        ".jpeg": "./Photos",
+        ".Jpeg": "./Photos",
+        ".gif": "./Photos",
+        ".HEIC": "./Photos",
+        ".heic": "./Photos",
+        ".JPG": "./Photos",
+        ".jp2": "./Photos",
+        ".png": "./Photos",
+        ".svg": "./Photos",
+        ".mov": "./Videos",
+        ".MOV": "./Videos",
+        ".mp4": "./Videos",
+        ".docx": "./Word",
+        ".odt": "./Word",
+        ".doc": "./Word",
+        ".pages": "./Word",
+        ".pptx": "./PowerPoint",
+        ".m4a": "./Audio",
+        ".wave": "./Audio",
+        ".wav": "./Audio"
+    }
+}
+if not os.path.exists(path_to_data_dir):
+    os.mkdir(path_to_data_dir)
+    os.mkdir(CONFIGS_DIR)
+    with open(os.path.join(CONFIGS_DIR, "default.json"), 'w') as f:
+        json.dump(default_config, f, indent=2)
 
 
 def get_config_path(name):
@@ -13,12 +52,12 @@ def get_list_of_configurations():
 
 
 def print_list_configurations():
-    configs = get_list_of_configurations()
+    configs = [c.replace('.json', '') for c in get_list_of_configurations()]
     print(*configs, sep="\n")
 
 
 def open_configuration(name):
-    os.system("open -e " + get_config_path(name))  # TODO: Doesn't work for other OS than MACOS
+    os.system(f"open -e \"{get_config_path(name)}\"")  # TODO: Doesn't work for other OS than MACOS
 
 
 def print_configuration(name):
