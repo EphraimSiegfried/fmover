74dbdab780a4ccd28179b4c5702fd3ce13d65d21
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2022-11-25 11:11:42 +0100

    Add logger

--- fmover/__main__.py
+++ fmover/__main__.py
@@ -1,31 +1,61 @@
 import fmover.cli as cli
 import fmover.configs as configs
 from fmover.mover import Mover
+import logging
 
 
 def main():
+    # Parse arguments
     args = cli.enable_argument_parser().parse_args()
+
+    # The user wants to list all configurations
     if args.listConfigs:
         configs.print_list_configurations()
-    elif args.openConfig:
+
+    # The user wants to open a given configuration file in a editor
+    if args.openConfig:
         configs.open_configuration(args.openConfig)
-    elif args.printConfig:
+
+    # The user wants to print the content of a given configuration file
+    if args.printConfig:
         configs.print_configuration(args.printConfig)
-    elif args.createConfig:
+
+    # The user wants to create a new configuration file with a given basename
+    if args.createConfig:
         configs.create_configuration(args.createConfig)
-    elif args.deleteConfig:
+
+    # The user wants to delete a configuration file with a given basename
+    if args.deleteConfig:
         answer = input(f"Are you sure to delete {args.deleteConfig}? (y/n)")
         while answer != "y" and answer != "n":
             answer = input("Please enter \"y\" or \"n\" ")
         if answer == "y":
             configs.delete_configuration(args.deleteConfig)
-    elif args.moveOneFile:
+
+    # The user wants to move a single file based on a given configuration and a given file path
+    if args.moveOneFile:
         file_path, config = tuple(args.moveOneFile)
         Mover(configs.get_config_path(config)).move_file(file_path, not args.silent)
-    elif args.moveAllFilesInFolder:
+
+    # The user wants to move all files in a directory based on a given configuration and a given directory path
+    if args.moveAllFilesInFolder:
         dir_path, config = tuple(args.moveAllFilesInFolder)
         Mover(configs.get_config_path(config)).move_files_in_dir(dir_path, not args.silent)
 
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    file_handler = logging.FileHandler("fmover.log")
+    # Create handlers
+    console_handler = logging.StreamHandler()
+    file_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.INFO)
+    # Create formatters and add it to handlers
+    file_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    console_format = logging.Formatter("%(message)s")
+    file_handler.setFormatter(file_format)
+    console_handler.setFormatter(console_format)
+    # Add handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
     main()
