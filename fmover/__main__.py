e1078493ada47983f0b508d4dc2aef91056ae375
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2022-11-26 16:10:14 +0100

    Restructured code for better readability

--- fmover/__main__.py
+++ fmover/__main__.py
@@ -1,61 +1,92 @@
+import os.path
+
 import fmover.cli as cli
-import fmover.configs as configs
+from fmover.configs import MoveConfigsHandler
 from fmover.mover import Mover
-import logging
+import appdirs
+from fmover.base_logger import logger
 
 
 def main():
     # Parse arguments
     args = cli.enable_argument_parser().parse_args()
+    # Initialize Configuration Handler
+    c_handler = MoveConfigsHandler(appdirs.user_config_dir("fmover"))
 
     # The user wants to list all configurations
     if args.listConfigs:
-        configs.print_list_configurations()
+        c_handler.print_configs()
 
     # The user wants to open a given configuration file in a editor
     if args.openConfig:
-        configs.open_configuration(args.openConfig)
+        try:
+            c_handler.open_config(args.openConfig)
+        except (FileNotFoundError, OSError) as e:
+            print(e)
+            exit(1)
 
     # The user wants to print the content of a given configuration file
     if args.printConfig:
-        configs.print_configuration(args.printConfig)
+        try:
+            c_handler.print_config_content(args.printConfig)
+        except FileNotFoundError as e:
+            print(e)
+            exit(1)
 
     # The user wants to create a new configuration file with a given basename
     if args.createConfig:
-        configs.create_configuration(args.createConfig)
+        try:
+            c_handler.create_config(args.createConfig)
+            print("Configuration file created. The new list of configurations is:")
+            c_handler.print_configs()
+        except FileExistsError as e:
+            input(f"Configuration file already exists. Do you want to overwrite it? [y/N] ")
+            if input().lower() == "y":
+                c_handler.delete_config(args.createConfig)
+                c_handler.create_config(args.createConfig)
 
     # The user wants to delete a configuration file with a given basename
     if args.deleteConfig:
-        answer = input(f"Are you sure to delete {args.deleteConfig}? (y/n)")
-        while answer != "y" and answer != "n":
-            answer = input("Please enter \"y\" or \"n\" ")
-        if answer == "y":
-            configs.delete_configuration(args.deleteConfig)
+        try:
+            answer = input(f"Are you sure to delete {args.deleteConfig}? (y/n)")
+            while answer != "y" and answer != "n":
+                answer = input("Please enter \"y\" or \"n\" ")
+            if answer == "y":
+                c_handler.delete_config(args.deleteConfig)
+                print("Configuration file deleted. The new list of configurations is:")
+                c_handler.print_configs()
+        except FileNotFoundError as e:
+            print(e)
+            exit(1)
 
     # The user wants to move a single file based on a given configuration and a given file path
     if args.moveOneFile:
-        file_path, config = tuple(args.moveOneFile)
-        Mover(configs.get_config_path(config)).move_file(file_path, not args.silent)
+        try:
+            file_path, config = tuple(args.moveOneFile)
+            if not os.path.isabs(file_path):
+                file_path = os.path.abspath(file_path)
+            Mover(c_handler.get_config_path(config)).move_file(file_path, not args.silent, args.force)
+        except (ValueError, FileNotFoundError) as e:
+            # The user did not provide a valid configuration file
+            logger.error(e)
+            exit(1)
+        except FileExistsError as e:
+            print(e)
+            pass
 
     # The user wants to move all files in a directory based on a given configuration and a given directory path
     if args.moveAllFilesInFolder:
-        dir_path, config = tuple(args.moveAllFilesInFolder)
-        Mover(configs.get_config_path(config)).move_files_in_dir(dir_path, not args.silent)
+        try:
+            dir_path, config = tuple(args.moveAllFilesInFolder)
+            Mover(c_handler.get_config_path(config)).move_files_in_dir(dir_path, not args.silent, args.force)
+        except (ValueError, FileNotFoundError) as e:
+            # The user did not provide a valid configuration file
+            print(e)
+            exit(1)
+        except FileExistsError as e:
+            print(e)
+            pass
 
 
 if __name__ == '__main__':
-    logger = logging.getLogger(__name__)
-    file_handler = logging.FileHandler("fmover.log")
-    # Create handlers
-    console_handler = logging.StreamHandler()
-    file_handler.setLevel(logging.DEBUG)
-    console_handler.setLevel(logging.INFO)
-    # Create formatters and add it to handlers
-    file_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    console_format = logging.Formatter("%(message)s")
-    file_handler.setFormatter(file_format)
-    console_handler.setFormatter(console_format)
-    # Add handlers to the logger
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
     main()
