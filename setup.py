87fbf2a1d64748546203f5d656f1797b7cd41982
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2022-11-27 10:12:55 +0100

    Add licence and long description

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
-
+with open('README.md') as f:
+    long_description = f.read()
 setup(
     name='fmover',
     version='0.0.1',
     description='Move files based on file properties and given criteria',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Ephraim Siegfried',
     author_email='ephraim.siegfried@hotmail.com',
+    url="https://github.com/EphraimSiegfried/fmover",
+    license="MIT",
+    keywords=["filemanager","filemover","mover"],
     package_dir={'fmover': 'fmover'},
     python_requires=">=3.6.0",
     packages=find_packages(exclude=["tests"]),
